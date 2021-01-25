d61102399321886f1f7718a830fe5dffc1cd2e4a
Zofia Glapa <zofia.glapa@student.uj.edu.pl>
2021-01-25 12:12:22 +0100

    run.sh fix

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JAVAFX="javafx-sdk-15.0.1/lib/"
 
-java --module-path $JAVAFX --add-modules=javafx.controls --add-modules=javafx.media -cp out sample.Main
+java --module-path $JAVAFX --add-modules=javafx.controls --add-modules=javafx.media -cp out controller.Main
